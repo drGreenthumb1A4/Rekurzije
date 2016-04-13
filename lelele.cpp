659ba1d831bef63d4f7bdd4d58d34780871fa115
drGreenthumb1A4 <filip.pilot@gmail.com>
2016-04-13 11:21:18 +0200

    Update lelele.cpp

--- lelele.cpp
+++ lelele.cpp
@@ -10,5 +10,6 @@ int main(){
 int ubaci0(int x, int pot, int zadnja){
 	if(x==0) return 0;
 	if(x%10==zadnja) return x%10*10*pot + ubaci0(x/10, pot*100, x%10);
+	// moramo potenciju pomnozit sa sto da se ne bi preklopilo sa sljedecim brojem
 	return x%10*pot + ubaci0(x/10, pot*10, x%10);
 }
