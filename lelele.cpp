d780448570056014f946580e0c556db9c18e2e33
drGreenthumb1A4 <filip.pilot@gmail.com>
2016-04-13 11:19:41 +0200

    Zero

---
+++ lelele.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+int ubaci0(int, int, int);
+int main(){
+	int a;
+	scanf("%d", &a);
+	int rez = ubaci0(a, 1, -1);
+	printf("%d", rez);
+	return 0;
+}
+int ubaci0(int x, int pot, int zadnja){
+	if(x==0) return 0;
+	if(x%10==zadnja) return x%10*10*pot + ubaci0(x/10, pot*100, x%10);
+	return x%10*pot + ubaci0(x/10, pot*10, x%10);
+}
